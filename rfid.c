b83a926b92b2e3153e3ac584c988f33aeb0ba619
Andraz Brodnik <brodul@gmx.com>
2011-09-03 00:05:19 +0200

    easy-brodul hacksession

--- rfid.c
+++ rfid.c
@@ -25,7 +25,6 @@ http://www.easysw.com/~mike/serial/
 #include <fcntl.h>   /* File control definitions */
 #include <errno.h>   /* Error number definitions */
 #include <termios.h> /* POSIX terminal control definitions */
-#include <math.h>    /*The math lib*/
 
 /*
  * 'open_port()' - Open serial port 1.
@@ -39,8 +38,7 @@ int open_port(void)
 
     fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY); /*Temporaly change to USB0*/
 
-    if (fd == -1)
-    {                                              /* Could not open the port */
+    if (fd == -1) {                                              /* Could not open the port */
         fprintf(stderr, "open_port: Unable to open /dev/ttyUSB0 - %s\n",
                 strerror(errno));
     }
@@ -55,27 +53,36 @@ int checksum_calculation(unsigned char * ch)
 {
     unsigned short int i;
     short int state_ch;
-    unsigned short int alfa; /*Temp var for calculating checksum*/
+    /*unsigned short int alfa; [>Temp var for calculating checksum<]*/
     unsigned short int tempsum; /*Temp var for calculating checksum*/
 
-    ((char *) (&tempsum))[0] = ch[1];
-    ((char *) (&tempsum))[1] = ch[2];
+    /*((char *) (&tempsum))[0] = ch[1];*/
+    /*((char *) (&tempsum))[1] = ch[2];*/
 
-    for(i = 2;i < 6;i++)
+    /*for(i = 2;i < 6;i++)*/
+    /*{*/
+    /*((char *) (&alfa))[0] = ch[i * 2 - 1];*/
+    /*((char *) (&alfa))[1] = ch[i * 2];*/
+    /*tempsum = alfa ^ tempsum;*/
+    /*}*/
+
+    unsigned char tmp=0;
+    for (i=0;i<5;i++)
     {
-        ((char *) (&alfa))[0] = ch[i * 2 - 1];
-        ((char *) (&alfa))[1] = ch[i * 2];
-        tempsum = alfa ^ tempsum;
+        tmp ^= ch[i];
     }
 
-    if ( ch[11] == ((char *) (&tempsum))[0] && ch[12] == (((char *) (&tempsum))[1]) )
-    state_ch = 1;
-    else
-    state_ch = 0;
+    tempsum = tmp;
 
+    if (ch[5] == tmp)
+        state_ch = 1;
+    else
+        state_ch = 0;
+
+    #ifdef DEBUG
     printf("%x\n",tempsum);
-    printf("%x\n",ch[11]);
-    printf("%x\n",ch[12]);
+    printf("%x\n",ch[5]);
+    #endif
 
     return state_ch;
 }
@@ -86,7 +93,9 @@ void main()
     int mainfd=0;                                            /* File descriptor */
     int short i;
     int short state;
-    unsigned char chout[13];
+    unsigned char chout[15];
+    unsigned char hextable[7];
+    chout[14] = 0 ;
     struct termios options;
 
     mainfd = open_port();
@@ -113,10 +122,13 @@ void main()
 
     int r=-1; /*The read object*/
     int counter=0; /*Byte count for reading */
-    unsigned short int alfa; /*Temp var for calculating checksum*/
-    unsigned short int beta; /*Temp var for calculating checksum*/
-    unsigned short int tempsum; /*Temp var for calculating checksum*/
-    unsigned long int result; /*Printed ID on RFID tag*/
+    /*unsigned short int alfa; [>Temp var for calculating checksum<]*/
+    /*unsigned short int beta; [>Temp var for calculating checksum<]*/
+    /*unsigned short int tempsum; [>Temp var for calculating checksum<]*/
+    unsigned int result; /*Printed ID on RFID tag*/
+    unsigned char tmpa=0;
+    unsigned char tmpb=0;
+
 
     while (1)
     {
@@ -137,29 +149,50 @@ void main()
             usleep(20000);
         }
 
+        for(i=1;i<14;i=i+2)
+        {
+            /*Translation from ASCII to hex*/
+            if (47 < chout[i] && chout[i] < 58)
+                tmpa = chout[i] - 48;
+            if (64 < chout[i] && chout[i] < 71)
+                tmpa = chout[i] - 55;
+
+            if (47 < chout[i+1] && chout[i+1] < 58)
+                tmpb = chout[i+1] - 48;
+            if (64 < chout[i+1] && chout[i+1] < 71)
+                tmpb = chout[i+1] - 55;
+
+            hextable[i/2] = (tmpa << 4) + tmpb;
+            #ifdef DEBUG
+            printf("H:%i %x\n", i/2, hextable[i/2]);
+            #endif
+
+        }
+
         counter = 0;
 
         if (chout[0] == 0x02)
         {
-            state = checksum_calculation(chout);
-            printf("%d", state);
 
-            /*if (state)*/
-            if (1)
+            state = checksum_calculation(hextable);
+            if (state)
             {
+                #ifdef DEBUG
                 printf("\nChecksum OK\n");
-                for(i=3;i<11;i++)
-                {
-                    /*Translation from ASCII to hex*/
-                    if (47 < chout[i] && chout[i] < 58)
-                        chout[i] = chout[i] - 48;
-                    if (64 < chout[i] && chout[i] < 71)
-                        chout[i] = chout[i] - 55;
-                    /*Dirty*/
-                    result += pow(16,10-i) * chout[i];
 
+                printf("%s\n", chout);
+                for(i=0;i<14;i++)
+                {
+                    printf("%x-", chout[i]);
+                    printf("%c:", chout[i]);
                 }
-                printf("%lu\n", result);
+                printf("\n");
+                #endif
+
+                result = ((  hextable[1] << 24) | ((  hextable[2] << 16) ) | ((  hextable[3]  << 8) ) | (hextable[4] ));
+
+                printf("%i\n", result);
+
             }
             else printf("Checksum BAD :(");
 
