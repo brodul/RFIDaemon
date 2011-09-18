540425c64d73a72715222791a22081f0296eb1a3
Andraz Brodnik <brodul@gmx.com>
2011-09-18 23:29:30 +0200

    JSON sender

--- ssl.c
+++ ssl.c
@@ -1,42 +1,48 @@
 #include <stdio.h>
-#include <curl/curl.h>
 #include <string.h>
+#include <curl/curl.h>
 
 int main(void)
 {
-    CURL *curl;
-    CURLcode res;
-    struct curl_httppost *formpost=NULL;
-    struct curl_httppost *lastptr=NULL;
-    struct curl_slist *headerlist=NULL;
-      static const char buf[] = "Expect:";
+  CURL *curl;
+  CURLcode res;
+  struct curl_slist *headers=NULL;
+  headers = curl_slist_append(headers, "Content-Type: application/json");
+  char json[128];
+  curl = curl_easy_init();
+  if(curl) {
+    /* First set the URL that is about to receive our POST. This URL can
+       just as well be a https:// URL if that is what should receive the
+       data. */
+
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L );
+    /*curl_easy_setopt(curl, CURLOPT_URL, "https://openvz.brodul.org");*/
+    curl_easy_setopt(curl, CURLOPT_URL, "localhost/listener.php");
+    /*curl_easy_setopt(curl, CURLOPT_URL, "localhost");*/
+    /*curl_easy_setopt(curl, CURLOPT_URL, "http://www.postbin.org/qpss4f");*/
+    curl_easy_setopt(curl, CURLOPT_PORT, 8000 );
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+    strcpy (json,"{ ");
+    strcat (json,"\"mac\" : ");
+    strcat (json,"\"lol\"");
+    strcat (json,", \"rfid\" : ");
+    strcat (json,"\"lol\"");
+    strcat (json,"}");
 
 
-    curl = curl_easy_init();
-    if(curl)
-    {  headerlist = curl_slist_append(headerlist, buf);
 
-        curl_easy_setopt(curl, CURLOPT_URL, "https://openvz.brodul.org/");
-        curl_easy_setopt(curl, CURLOPT_PORT, 443);
-        curl_easy_setopt(curl, CURLOPT_POST, 1L);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-        /*curl_easy_setopt(curl, CURLOPT_CAINFO, );*/
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L );
+    /* Now specify the POST data */
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json );
 
-        curl_formadd(&formpost,
-            &lastptr,
-            CURLFORM_COPYNAME, "rfid",
-            CURLFORM_ARRAY, "blabla",
-            CURLFORM_END);
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    printf("%d", res);
 
-        curl_formadd(&formpost,
-            &lastptr,
-            CURLFORM_COPYNAME, "MAC",
-            CURLFORM_ARRAY, "blabla",
-            CURLFORM_END);
-
-        res = curl_easy_perform(curl);
-        printf("%d", res);
-        curl_easy_cleanup(curl);
-    }
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+  return 0;
 }
+
